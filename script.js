e6a97f6245540307f7c03b8b0c4e3f3c2d9f3499
vegetason <irakozepirlo@gmail.com>
2024-01-09 13:35:04 +0200

    Add a user-interface to the project.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+function theGame(){
+    let paper=document.querySelector('#paper')
+let rock=document.querySelector('#Rock')
+let scissors=document.querySelector('#scissors')
+let playerSelection;
+let screen=document.querySelector('#screen')
+let nbrOfRounds=0;
+let playerWins=0;
+let compWins=0;
+let selection=document.createElement('p')
+let theWinner=document.createElement('p')
+screen.appendChild(selection)
+screen.appendChild(theWinner)
+paper.addEventListener('click',()=>{
+    playerSelection='paper';
+    nbrOfRounds++;
+    game();
+})
+rock.addEventListener('click',()=>{
+    playerSelection='rock';
+    nbrOfRounds++
+    game();
+})
+scissors.addEventListener('click',()=>{
+    playerSelection='scissors';
+    nbrOfRounds++;
+    game();
+})
 function getComputerChoice(){
     let x=Math.floor(Math.random()*3)+1;
     if (x===1) return 'rock';
@@ -5,26 +33,56 @@ function getComputerChoice(){
     else if(x===3) return 'scissors';
 }
 function playRound(playerSelection,computerSelection){
-  if(playerSelection===computerSelection) return 'it is a tie';
-  else if(playerSelection==='scissors' && computerSelection==='paper') return 'player won'
-  else if(playerSelection==='rock' && computerSelection==='paper') return 'computer won';
-  else if(playerSelection==='paper' && computerSelection==='rock') return 'player won'
-  else if(playerSelection==='scissors' && computerSelection==='rock') return 'computer won'
-  else if(playerSelection==='paper' && computerSelection==='scissors') return 'computer won'
-  else if(playerSelection==='rock' && computerSelection==='scissors') return 'player won'
+  if(playerSelection===computerSelection) {
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent='it is a tie.'
+    return 'it is a tie';
+  }
+  else if(playerSelection==='scissors' && computerSelection==='paper') {
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent='player won';
+    return 'player won';
+  }
+  else if(playerSelection==='rock' && computerSelection==='paper') {
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent ='computer won';
+    return 'computer won';
+  }
+  else if(playerSelection==='paper' && computerSelection==='rock') {
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent='player won';
+    return 'player won'
+  }
+  else if(playerSelection==='scissors' && computerSelection==='rock'){
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent ='computer won';
+    return 'computer won';
+  }
+  else if(playerSelection==='paper' && computerSelection==='scissors'){
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent ='computer won';
+    return 'computer won';
+  }
+  else if(playerSelection==='rock' && computerSelection==='scissors'){
+    selection.textContent=`computer Selection is ${computerSelection} and playerSelection is ${playerSelection}`
+    theWinner.textContent='player won';
+    return 'player won';
+  }
 };
 function game(){
-    let p=0,c=0;
-    for(let i=0;i<5;i++){
-        let playerSelection=prompt('enter your choice');
-        let computerSelection=getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-        if(playRound(playerSelection,computerSelection)==='player won') p++;
-        else if(playRound(playerSelection,computerSelection)==='computer won') c++;
-        console.log(p,c)
+    let computerSelection=getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    if(playRound(playerSelection,computerSelection)==='player won') playerWins++;
+    else if(playRound(playerSelection,computerSelection)==='computer won') compWins++;
+    if(nbrOfRounds===5){
+        if(playerWins>compWins) screen.textContent= `Player won, the score is ${playerWins}:${compWins}`;
+        else if(playerWins<compWins) screen.textContent =`Computer won, the score is ${playerWins}:${compWins}`;
+        else if(playerWins===compWins) screen.textContent='It\'s a miraculous tie';
+        nbrOfRounds=0;
+        playerWins=0; 
+        compWins=0;
+        theGame();
     }
-    if(p>c) return `Player won, the score is ${p}:${c}`;
-    else if(c>p) return `Computer won, the score is ${p}:${c}`;
-    else if(c===p) return 'It\'s a miraculous tie'
 }
-console.log(game());
+}
+theGame();
